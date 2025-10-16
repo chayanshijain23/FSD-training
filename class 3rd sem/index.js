232e3d76c09fd77acbf7d74c40c1da69c302aa57
chayanshijain23 <chayanshijain23032004@gmail.com>
2025-10-16 15:35:57 +0530

    Add files via upload

---
+++ class 3rd sem/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+document.write("Hello World");
+document.writeln("<h1>Hello World</h1>");
+
+console.log(a);
+var a = 10;
+console.log(a);
+var a=20;
+console.log(a);
+let b=30;
